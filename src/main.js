eb6d47b53b8055ca9c44e2333c6668bae1f4ab6c
abhay333d <abhaydandge333@gmail.com>
2025-03-25 17:02:07 +0530

    applied the textures on the planets

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three';
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
 import gsap from 'gsap';
 
 // Initialize scene, camera and renderer
@@ -11,16 +12,49 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true
 });
 
+
+
 const radius = 1.3;
 const segments = 64;
 const colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00];
+const textures = [
+  './csilla/color.png',
+  './earth/map.jpg',
+  './venus/map.jpg',
+  './volcanic/color.png'
+]
 const spheres = new THREE.Group();
 const orbitRadius = 4.5;
 
+// Create a large sphere for the starfield background
+const starfieldGeometry = new THREE.SphereGeometry(50, 64, 64);
+const starfieldTexture = new THREE.TextureLoader().load('./stars.jpg');
+starfieldTexture.colorSpace = THREE.SRGBColorSpace;
+starfieldTexture.wrapS = THREE.RepeatWrapping;
+starfieldTexture.wrapT = THREE.RepeatWrapping;
+const starfieldMaterial = new THREE.MeshStandardMaterial({
+  map: starfieldTexture,
+  side: THREE.BackSide, // Render on the inside of the sphere
+  opacity: 0.7,
+  transparent: true
+});
+const starfield = new THREE.Mesh(starfieldGeometry, starfieldMaterial);
+scene.add(starfield);
+
+
+
+
 for (let i = 0; i < 4; i++) {
+
+  const textureLoader = new THREE.TextureLoader();
+  const texture = textureLoader.load(textures[i]);
+  texture.colorSpace = THREE.SRGBColorSpace;
+
   const geometry = new THREE.SphereGeometry(radius, segments, segments);
-  const material = new THREE.MeshBasicMaterial({ color: colors[i] });
+  const material = new THREE.MeshStandardMaterial({ map: texture });
   const sphere = new THREE.Mesh(geometry, material);
+
+
 
   const angle = (i * Math.PI * 2) / 4;
   sphere.position.x = orbitRadius*Math.cos(angle);
@@ -32,17 +66,17 @@ spheres.rotation.x = 0.1;
 spheres.position.y = -0.8;
 scene.add(spheres);
 
+const loader = new RGBELoader();
+loader.load('https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/moonlit_golf_1k.hdr', (texture) => {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.environment = texture;
+});
+
+
 // Set renderer size and pixel ratio
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
  
-
-// Add lights
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(5, 5, 5);
-scene.add(directionalLight);
 
 // Position camera
 camera.position.z = 9;
